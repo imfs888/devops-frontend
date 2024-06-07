ff7a4ad2eb7ef53b3b9552fe76b6cb6b62ce5220
Faizan Siddiqui <faizan.siddiqui@gaditek.com>
2024-06-08 03:21:04 +0500

    Container deployment updated.

--- scripts/deploy-container.sh
+++ scripts/deploy-container.sh
@@ -7,7 +7,7 @@ export AWS_ACCOUNT_ID="590183909822"
 export REPOSITORY_URI="590183909822.dkr.ecr.us-east-1.amazonaws.com/frontend"
 export IMAGE_TAG="latest"
 VITE_BACKEND_URL=$(aws elbv2 describe-load-balancers --query "LoadBalancers[?contains(DNSName, 'devops-terraform-project-be-lb')].[DNSName]" --output text)
-export VITE_BACKEND_URL
+export VITE_BACKEND_URL="http://${VITE_BACKEND_URL}"
 
 # ECR login
 aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com
