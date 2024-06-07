46d962b8feaa00b85e1c195aae8c682726218697
Faizan Siddiqui <faizan.siddiqui@gaditek.com>
2024-06-08 04:29:12 +0500

    Testing commit.

--- src/components/course-form.tsx
+++ src/components/course-form.tsx
@@ -53,7 +53,7 @@ export const CourseForm = () => {
 
     return (
         <Container maxW="lg" mt={8}>
-            <Heading textAlign='center'>Course Form</Heading>
+            <Heading textAlign='center'>Course Enrollment Form</Heading>
             <Box p={6} boxShadow="md" borderRadius="md">
                 <form onSubmit={handleSubmit}>
                     <FormControl id="title" mb={4}>
